9495dc731e477ef92e5557e2e57ba8467417678f
Bastian Greshake <bgreshake@googlemail.com>
2016-02-16 14:03:21 +0100

    minor changes to graph script

--- plotGenotypes.r
+++ plotGenotypes.r
@@ -3,5 +3,9 @@ library(ggthemes)
 
 genotype <- read.csv(file="genotypenumber.csv",sep="\t",head=F)
 genotype$date <- as.POSIXct(as.character(genotype$V2),format="%d.%m.%Y %H:%M")
-ggplot(genotype,aes(genotype$date,genotype$V1)) + geom_smooth() + theme_light() + scale_x_datetime("Date") + scale_y_continuous("# of Genotypes")
+ggplot(genotype,aes(genotype$date,genotype$V1)) +
+  geom_smooth() +
+  theme_light() +
+  scale_x_datetime("Date") +
+  scale_y_continuous("# of Genotypes")
 ggsave("genotypes.pdf")
